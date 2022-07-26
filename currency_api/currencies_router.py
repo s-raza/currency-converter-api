4a0632c1797090404746ec4e6943de0265d41f3b
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-07-27 00:32:12 +0400

    API: Use refactored session, engine objects

--- currency_api/currencies_router.py
+++ currency_api/currencies_router.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Union
 
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 
 from db.currency_db import CurrencyDB
 from db.models import CurrencyUpdates
@@ -11,21 +11,14 @@ from .user_auth_router import UserModel, get_current_active_user
 currencies_router = APIRouter()
 
 
-async def close_conn(conn: CurrencyDB) -> None:
-    await conn.session.close()
-    await conn.engine.dispose()
-
-
 @currencies_router.get("/currencies/rate/{curr_code}")
 async def get_currency_code_rate(
     curr_code: str,
-    background_tasks: BackgroundTasks,
     on_date: Union[str, None] = None,
     currency_db: CurrencyDB = Depends(get_currency_db),
     auth_user: UserModel = Depends(get_current_active_user),
 ) -> Dict[str, Any]:
 
-    background_tasks.add_task(close_conn, currency_db)
     try:
         currency: CurrencyUpdates = await currency_db.get_rate_on_date(
             curr_code, on_date
@@ -48,13 +41,11 @@ async def convert_currency_rate(
     from_code: str,
     to_code: str,
     amount: float,
-    background_tasks: BackgroundTasks,
     on_date: Union[str, None] = None,
     currency_db: CurrencyDB = Depends(get_currency_db),
     auth_user: UserModel = Depends(get_current_active_user),
 ) -> Dict[str, Any]:
 
-    background_tasks.add_task(close_conn, currency_db)
     try:
         converted: Dict[str, Any] = await currency_db.convert_rate(
             from_code, to_code, amount, on_date
@@ -75,12 +66,10 @@ async def convert_currency_rate(
 
 @currencies_router.get("/currencies")
 async def get_all_currencies(
-    background_tasks: BackgroundTasks,
     currency_db: CurrencyDB = Depends(get_currency_db),
     auth_user: UserModel = Depends(get_current_active_user),
 ) -> Dict[str, Any]:
 
-    background_tasks.add_task(close_conn, currency_db)
     currencies: List[str] = await currency_db.get_currency_codes()
     await currency_db.session.close()
     return {"success": True, "currencies": currencies}
