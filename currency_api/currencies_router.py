2e0256998acee750d41ff837070b9d39fad0d802
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-07-28 14:51:32 +0400

    API: Refactor request path, params
    into separate module

--- currency_api/currencies_router.py
+++ currency_api/currencies_router.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List
 
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -7,6 +7,7 @@ from db.models import CurrencyUpdates
 
 from .dependencies.db import get_currency_db
 from .models import errors as err
+from .models import request as req
 from .models import response as resp
 from .user_auth_router import UserModel, get_current_active_user
 
@@ -19,26 +20,24 @@ currencies_router = APIRouter()
     responses=err.get_responses(err.all_errors),
 )
 async def get_currency_code_rate(
-    curr_code: str,
-    on_date: Union[str, None] = None,
+    currency_in: req.CurrencyIn = Depends(),
     currency_db: CurrencyDB = Depends(get_currency_db),
     auth_user: UserModel = Depends(get_current_active_user),
 ) -> Dict[str, Any]:
 
     try:
-        currency: CurrencyUpdates = await currency_db.get_rate_on_date(
-            curr_code, on_date
+        db: CurrencyUpdates = await currency_db.get_rate_on_date(
+            currency_in.curr_code, currency_in.on_date
         )
     except ValueError as e:
         errcode, errtxt = e.args[0]
         raise HTTPException(status_code=errcode, detail=errtxt)
-    await currency_db.session.close()
     return {
         "success": True,
-        "currency_code": curr_code,
-        "base_currency": currency.date_updated.base_currency.code,
-        "rate": currency.rate,
-        "last_updated": currency.date_updated.created,
+        "currency_code": currency_in.curr_code,
+        "base_currency": db.date_updated.base_currency.code,
+        "rate": db.rate,
+        "last_updated": db.date_updated.created,
     }
 
 
@@ -48,28 +47,27 @@ async def get_currency_code_rate(
     responses=err.get_responses(err.all_errors),
 )
 async def convert_currency_rate(
-    from_code: str,
-    to_code: str,
-    amount: float,
-    on_date: Union[str, None] = None,
+    convert_in: req.ConvertIn = Depends(),
     currency_db: CurrencyDB = Depends(get_currency_db),
     auth_user: UserModel = Depends(get_current_active_user),
 ) -> Dict[str, Any]:
 
     try:
         converted: Dict[str, Any] = await currency_db.convert_rate(
-            from_code, to_code, amount, on_date
+            convert_in.from_code,
+            convert_in.to_code,
+            convert_in.amount,
+            convert_in.on_date,
         )
     except ValueError as e:
         errcode, errtxt = e.args[0]
         raise HTTPException(status_code=errcode, detail=errtxt)
 
-    await currency_db.session.close()
     return {
         "success": True,
-        "from_currency_code": from_code,
-        "to_currency_code": to_code,
-        "amount": amount,
+        "from_currency_code": convert_in.from_code,
+        "to_currency_code": convert_in.to_code,
+        "amount": convert_in.amount,
         **converted,
     }
 
@@ -81,5 +79,4 @@ async def get_all_currencies(
 ) -> Dict[str, Any]:
 
     currencies: List[str] = await currency_db.get_currency_codes()
-    await currency_db.session.close()
     return {"success": True, "currencies": currencies}
