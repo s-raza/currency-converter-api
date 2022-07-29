3272f0a4f774358f2a3595e80a9f15b2a7485dc9
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-07-29 20:38:45 +0400

    Reorganise files

--- currency_api/utils.py
+++ currency_api/utils.py
@@ -2,6 +2,14 @@ from fastapi import Request
 
 
 def request_url(request: Request) -> str:
+    """
+    Utility function to get combination of API path and query
+
+    :param request: :obj:`fastapi.Request` object.
+    :type request: required
+
+    :return: :obj:`str`
+    """
     query = request.url.components.query
     path = request.url.components.path
 
