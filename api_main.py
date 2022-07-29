9cd68b2246f321df9d1e66cea6a89839577239ac
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-07-29 10:41:15 +0400

    Add redis cache as middleware

--- api_main.py
+++ api_main.py
@@ -1,7 +1,12 @@
+from typing import Any
+
+import aioredis
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from starlette.concurrency import iterate_in_threadpool
 
 from currency_api.currencies_router import currencies_router
+from currency_api.middleware import utils
 from currency_api.user_auth_router import user_router
 from db.currency_db import CurrencyDB
 from db.database import get_db
@@ -9,7 +14,44 @@ from db.utils import wait_for_db
 from settings import settings as cfg
 
 app = FastAPI()
-app.include_router(currencies_router)
+currencies_prefix = "currencies"
+
+redis = aioredis.from_url(
+    f"redis://{cfg().redis.host}:{cfg().redis.port}", decode_responses=True
+)
+
+
+@app.middleware("http")
+async def redis_cache(request: Request, call_next: Any) -> Response:
+
+    url = utils.request_url(request)
+
+    if currencies_prefix in url:
+        from_redis = await redis.execute_command("GET", url)
+
+        if not from_redis:
+            response = await call_next(request)
+            response_body = [chunk async for chunk in response.body_iterator]
+            response.body_iterator = iterate_in_threadpool(iter(response_body))
+
+            resp_str = response_body[0].decode()
+            await redis.execute_command(
+                "SETEX", url, cfg().redis.expire_seconds, resp_str
+            )
+            response_to_send = response
+        else:
+            response_to_send = Response(
+                content=from_redis,
+                status_code=200,
+                media_type="application/json",
+            )
+
+        return response_to_send
+
+    return await call_next(request)
+
+
+app.include_router(currencies_router, prefix=f"/{currencies_prefix}")
 app.include_router(user_router)
 
 
