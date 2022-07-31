1a9368fffb936748789235b3fa75b84a6769ea44
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-07-31 09:49:05 +0400

    Tests:API: use settings from config
    instead of hard coding them.

--- tests/fixtures/api.py
+++ tests/fixtures/api.py
@@ -1,6 +1,8 @@
 import pytest_asyncio
 from httpx import AsyncClient, Auth, Response
 
+from settings import settings as cfg
+
 
 class PasswordAuth(Auth):
     def __init__(self, token):
@@ -14,15 +16,20 @@ class PasswordAuth(Auth):
 @pytest_asyncio.fixture(scope="module")
 async def api():
 
-    base_url = "http://localhost:8070"
+    port = cfg().api.startup.port
+
+    base_url = f"http://localhost:{port}"
     auth_client = AsyncClient(base_url=base_url)
+
+    user_settings = cfg().api.user
     token: Response = await auth_client.post(
-        f"{base_url}/token", data={"username": "user", "password": "pass123"}
+        f"{base_url}/token",
+        data={"username": user_settings.username, "password": user_settings.password},
     )
     await auth_client.aclose()
 
     auth = PasswordAuth(token.json()["access_token"])
 
-    client = AsyncClient(base_url=f"{base_url}/currencies", auth=auth)
+    client = AsyncClient(base_url=f"{base_url}/{cfg().api.prefix}", auth=auth)
     yield client
     await client.aclose()
