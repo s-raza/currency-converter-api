0a83ac836eea4ba2467b8f8066239d706070dbe8
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-08-11 19:56:34 +0400

    Test API: currency non existant

--- tests/test_api.py
+++ tests/test_api.py
@@ -45,3 +45,24 @@ async def test_latest_rate_for_currency_success(
     assert json.get("currency_code") == curr_code
     assert json.get("base_currency") == sr.base_currency2
     assert json.get("rate") == sr.rates2[curr_code]
+
+
+@pytest.mark.asyncio
+async def test_currency_non_existant(api: Dict[str, Union[CurrencyDB, AsyncClient]]):
+
+    db = api["db"]
+    client = api["client"]
+
+    await db.add_update(sr.base_currency, sr.rates)
+
+    curr_code = "ABC"
+
+    rate: Response = await client.get(f"/rate/{curr_code}")
+
+    json: Dict = rate.json()
+    success = json.get("success")
+    detail = json.get("detail")
+
+    assert success is None
+    assert detail is not None and detail == f"Currency code {curr_code} does not exist"
+    assert rate.status_code == 404
