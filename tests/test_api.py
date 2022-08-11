a9629611784f610efddc37c447874fcdb00cff92
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2022-08-11 18:21:33 +0400

    Test API: latest rate for currency code

--- tests/test_api.py
+++ tests/test_api.py
@@ -22,3 +22,26 @@ async def test_api_currencies_list(api: Dict[str, Union[CurrencyDB, AsyncClient]
 
     assert success is not None and success is True
     assert json.get("currencies") == list(sr.rates.keys())
+
+
+@pytest.mark.asyncio
+async def test_latest_rate_for_currency_success(
+    api: Dict[str, Union[CurrencyDB, AsyncClient]]
+):
+
+    db = api["db"]
+    client = api["client"]
+
+    await db.add_update(sr.base_currency, sr.rates)
+    await db.add_update(sr.base_currency2, sr.rates2)
+
+    curr_code = "AED"
+    rate: Response = await client.get(f"/rate/{curr_code}")
+
+    json: Dict = rate.json()
+    success = json.get("success")
+
+    assert success is not None and success is True
+    assert json.get("currency_code") == curr_code
+    assert json.get("base_currency") == sr.base_currency2
+    assert json.get("rate") == sr.rates2[curr_code]
