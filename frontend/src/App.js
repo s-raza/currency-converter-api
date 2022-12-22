be325f03bd3fbe92626bf61063a1c4f8ac2e4a4c
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2022-12-22 22:52:20 +0530

    React: add currency rates grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useCallback } from 'react';
 import CurrencyConverter from './components/currencyConverter';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
+import GridCurrencyRates from './components/currencyDataGrid';
+import Stack from '@mui/material/Stack';
 
 function App() {
   const [currencies, setCurrencies] = useState([]);
@@ -20,7 +22,11 @@ function App() {
     <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh"
     >
       {
-        currencies && currencies.success? <CurrencyConverter currencies={currencies.currencies}/> :
+        currencies && currencies.success?
+        <Stack spacing={2}>
+        <CurrencyConverter currencies={currencies.currencies}/>
+        <GridCurrencyRates />
+        </Stack> :
         <Box sx={{ display: 'flex' }}>
           <CircularProgress />
         </Box>
---
+++ frontend/src/components/currencyDataGrid.js
@@ -0,0 +1,52 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+import currencyFormatter from './utils';
+import Typography from '@mui/material/Typography';
+
+const colHeaders = [
+    {field: 'currCode', headerName: 'Code', width: 120},
+    {field: 'rate', headerName: 'Rate', width: 120},
+]
+
+const GridCurrencyRates = () => {
+    const [rates, setRates] = useState({})
+
+    const getRates = useCallback(async () => {
+        await fetch(`/currencies/rates`).then(res => res.json()).then(data => {
+            setRates(data);
+        });
+      }, [])
+
+    useEffect(() => {
+        getRates()
+      }, [getRates]);
+
+    let rows = []
+
+    if (rates && rates.success) {
+        for (const [key, value] of Object.entries(rates.rates)) {
+        rows.push({'id': key, 'currCode': key, 'rate': currencyFormatter(value, key)})
+        }
+    }
+
+      return (
+        <Box sx={{ height: 400, width: '100%' }}>
+            <Typography gutterBottom align='center'>
+                Current Rates, Base Currency: {rates.base_currency}
+            </Typography>
+          <DataGrid
+            rows={rows}
+            rowHeight={30}
+            headerHeight={35}
+            density='compact'
+            columns={colHeaders}
+            disableSelectionOnClick
+            experimentalFeatures={{ newEditingApi: true }}
+            sortModel={[{field: 'currCode', sort: 'asc'}]}
+          />
+        </Box>
+      )
+}
+
+export default GridCurrencyRates
