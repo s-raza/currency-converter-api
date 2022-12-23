a9e4e5348ad291e187b0ce8c83665494db899b87
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2022-12-23 20:14:57 +0530

    React: update rates in grid based on base_currency

--- frontend/src/components/currencyDataGrid.js
+++ frontend/src/components/currencyDataGrid.js
@@ -2,39 +2,45 @@ import React, { useState, useCallback, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import {currencyFormatter} from './utils';
-import Typography from '@mui/material/Typography';
+import { convertCurrency } from './utils';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const colHeaders = [
     {field: 'currCode', headerName: 'Code', width: 120},
     {field: 'rate', headerName: 'Rate', width: 120},
 ]
 
-const GridCurrencyRates = () => {
-    const [rates, setRates] = useState({})
+const GridCurrencyRates = ({rates}) => {
+    const [baseCurrency, setBaseCurrency] = useState(rates.base_currency)
+    const [rows, setRows] = useState([])
+    let currencyList = Object.keys(rates.rates).sort()
 
-    const getRates = useCallback(async () => {
-        await fetch(`/currencies/rates`).then(res => res.json()).then(data => {
-            setRates(data);
-        });
-      }, [])
+    const getRows = useCallback( () => {
+        let formatted_rows = []
+        for (const [key, value] of Object.entries(rates.rates)) {
+            formatted_rows.push({'id': key, 'currCode': key, 'rate': currencyFormatter(convertCurrency(value, rates.rates[baseCurrency]), key)})
+        }
+        setRows(formatted_rows)
+      }, [baseCurrency])
 
     useEffect(() => {
-        getRates()
-      }, [getRates]);
-
-    let rows = []
-
-    if (rates && rates.success) {
-        for (const [key, value] of Object.entries(rates.rates)) {
-        rows.push({'id': key, 'currCode': key, 'rate': currencyFormatter(value, key)})
-        }
-    }
+        getRows()
+      }, [baseCurrency, getRows]);
 
       return (
         <Box sx={{ height: 400, width: '100%' }}>
-            <Typography gutterBottom align='center'>
-                Current Rates, Base Currency: {rates.base_currency}
-            </Typography>
+        <Autocomplete
+                options={currencyList}
+                disableClearable
+                id="currencies-from"
+                value={baseCurrency}
+                onChange={ (event, newValue) => setBaseCurrency(newValue)}
+                sx={{ width: 100 }}
+                renderInput={(params) => (
+                <TextField {...params} variant="standard" />
+                )}
+            />
           <DataGrid
             rows={rows}
             rowHeight={30}
