8591b9d9d800848ae767efe849ee7217fdf30014
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2022-12-26 21:53:11 +0530

    React: basic log in/out button

--- frontend/src/components/pages/login.js
+++ frontend/src/components/pages/login.js
@@ -1,11 +1,29 @@
-import { Box, Typography } from "@mui/material"
+import { useState, useEffect, useCallback} from 'react';
+import {
+    Box,
+    Button,
+
+} from "@mui/material"
+
 
 const LoginPage = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [buttonText, setButtonText] = useState("Login")
+
+    const handleLogin = (event) => {
+        setLoggedIn(!loggedIn)
+    }
+
+    const updateButtonText = useCallback(
+        () => {
+            setButtonText(loggedIn? "Log Out": "Login")
+    },[loggedIn])
+
+    useEffect(updateButtonText, [loggedIn, updateButtonText])
+
     return (
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-            <Typography variant="h4" gutterBottom>
-                Login Page
-            </Typography>
+            <Button variant={loggedIn? "contained": "outlined"} onClick={handleLogin}>{buttonText}</Button>
         </Box>
     )
 }
