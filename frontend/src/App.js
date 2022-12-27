3dd0563aea7263c97cb74554cf895de05a02b98b
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2022-12-27 17:07:21 +0530

    React: add nav routes for login/logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import { useState, useEffect, useCallback } from 'react';
 import CurrencyConverter from './components/currencyConverter';
-import Box from '@mui/material/Box';
+import {Box} from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import GridCurrencyRates from './components/currencyDataGrid';
 import Stack from '@mui/material/Stack';
+import { useNavigate } from "react-router-dom";
+import { LoginButton } from './components/pages/login';
+import TopBar from './components/appBar';
 
 function App() {
+  let navigate = useNavigate();
   const [rates, setRates] = useState({});
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem('loggedIn') === 'true')
 
   let getRates = useCallback(async () => {
     await fetch('/currencies/rates').then(res => res.json()).then(data => {
@@ -14,11 +19,21 @@ function App() {
     });
   }, [])
 
+  const handleLogout = () => {
+    setLoggedIn(false)
+    localStorage.setItem('loggedIn', false)
+  }
+
   useEffect(() => {
+    if (!loggedIn){
+      return navigate("login");
+   }
     getRates()
-  }, [getRates]);
+  }, [loggedIn, getRates, navigate]);
 
   return (
+    <>
+    <TopBar loginButton={<LoginButton loggedIn={loggedIn} onClick={handleLogout} buttonText="Log Out" />}/>
     <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh"
     >
       {
@@ -32,6 +47,7 @@ function App() {
         </Box>
       }
     </Box>
+    </>
   );
 }
 
--- frontend/src/components/pages/login.js
+++ frontend/src/components/pages/login.js
@@ -4,9 +4,16 @@ import {
     Button,
 
 } from "@mui/material"
+import { useNavigate } from "react-router-dom";
 
+export const LoginButton = ({loggedIn, onClick, buttonText}) => {
+    return(
+        <Button variant={loggedIn? "contained": "outlined"} onClick={onClick}>{buttonText}</Button>
+    )
+}
 
 const LoginPage = () => {
+    let navigate = useNavigate();
     const [loggedIn, setLoggedIn] = useState(localStorage.getItem('loggedIn') === 'true')
     const [buttonText, setButtonText] = useState("Login")
 
@@ -18,13 +25,16 @@ const LoginPage = () => {
         () => {
             setButtonText(loggedIn? "Log Out": "Login")
             localStorage.setItem('loggedIn', loggedIn)
-    },[loggedIn])
+            if(loggedIn) {
+                return navigate("/");
+            }
+    },[loggedIn, navigate])
 
     useEffect(updateButtonText, [loggedIn, updateButtonText])
 
     return (
         <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-            <Button variant={loggedIn? "contained": "outlined"} onClick={handleLogin}>{buttonText}</Button>
+            <LoginButton loggedIn={loggedIn} onClick={handleLogin} buttonText={buttonText}/>
         </Box>
     )
 }
