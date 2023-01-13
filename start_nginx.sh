fbc87d1bb8c789dd23e37f18c39d59a57b2d2c5c
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2023-01-13 22:59:49 +0530

    Move nginx related files to root directory

--- start_nginx.sh
+++ start_nginx.sh
@@ -4,5 +4,5 @@ export D='$'
 
 /bin/sh -c envsubst \
 '$${NGINX_UPSTREAM} $${NGINX_SERVER_PORT} $${NGINX_API_PREFIX}' \
-< /etc/nginx/conf.d/currency-converter.conf.template > /etc/nginx/conf.d/default.conf
+< /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
