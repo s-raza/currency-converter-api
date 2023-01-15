4e4489da1532a6b0e021e482ab104069b1b6857c
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2023-01-15 18:56:17 +0530

    Settings: use .env location list for loading+
    Load settings using a list of .env path locations so that it works
    accross testing, containers and development. This is done since the path
    to .env changes between these environments. The path to .env is loaded
    by priority in the same order that they are in the list, the first path
    found is loaded.

--- backend/settings.py
+++ backend/settings.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict
 
-from pydantic import BaseModel, BaseSettings, ValidationError
+from pydantic import BaseModel, BaseSettings
 
 
 def get_conn_string(db_settings: Dict[str, Any]) -> str:
@@ -175,7 +175,7 @@ class Settings(BaseSettings):
     nginx: Nginx
 
     class Config:
-        env_file = ".env"
+        env_file = [".env", "../.env", "../../.env"]
         env_nested_delimiter = "__"
 
     @property
@@ -209,11 +209,4 @@ def settings() -> Settings:
     """
     Get settings object to import in other modules.
     """
-    try:
-        return Settings()
-    except ValidationError:
-        # When generating docs with sphinx ``.env`` is not found by pydantic as sphinx
-        # is running from the ``docs`` directory. We then load the ``.env`` from one
-        # directory above.
-        # TODO: Find a better solution.
-        return Settings(_env_file="../.env")
+    return Settings()
